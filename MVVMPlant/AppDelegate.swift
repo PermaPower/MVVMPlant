307784ac725e2e0f48614d75664096ac7e0c9e73
Regina Power <ReginaPower@MacBook-Pro.local>
2017-06-29 20:21:36 +1000

    CollectionView added

--- MVVMPlant/AppDelegate.swift
+++ MVVMPlant/AppDelegate.swift
@@ -13,9 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch...
+		// Override point for customization after application launch.
+		
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.makeKeyAndVisible()
+		
+		let layout = UICollectionViewFlowLayout()
+		
+		window?.rootViewController = UINavigationController(rootViewController: HomeViewController(collectionViewLayout: layout))
+		
 		return true
 	}
 
--- MVVMPlant/ViewController.swift
+++ MVVMPlant/ViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+	
+	let cellID = "cellID"
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
+		navigationItem.title = "Home"
+		
+		collectionView?.backgroundColor = UIColor.red
+		
+		collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+		
+	}
+	
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 5
+	}
+	
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+		
+		cell.backgroundColor = UIColor.blue
+		return cell
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return CGSize(width: view.frame.width, height: 200)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -22,5 +46,18 @@ class ViewController: UIViewController {
 	}
 
 
+}
+
+
+// Custom cell for collectionview to display plant data
+class PlantCell: UICollectionViewCell {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
 }
 
